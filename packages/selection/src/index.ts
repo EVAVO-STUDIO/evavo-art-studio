a82dc12e18736183881d1e8afc2ff53a59ab53dc
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-07-30 09:20:05 +1000

    feat: export candidate selection package

---
+++ packages/selection/src/index.ts
@@ -0,0 +1,6 @@
+export * from "./types.js";
+export * from "./validation.js";
+export * from "./features.js";
+export * from "./compare.js";
+export * from "./select.js";
+export * from "./promotion.js";
