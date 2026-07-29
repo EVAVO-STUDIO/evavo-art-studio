0658dab755674ea97973c70325da3742b9d77a47
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-07-29 19:24:53 +1000

    fix: type required sprite QA status classes

---
+++ apps/web/app/sprite-quality-workbenches.module.css.d.ts
@@ -0,0 +1,9 @@
+declare const classes: Readonly<{
+  gateFail: string;
+  gateWarning: string;
+  gateSkipped: string;
+  gatePass: string;
+  [className: string]: string;
+}>;
+
+export default classes;
