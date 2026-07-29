b3829036f28eaad9b95a145a1c57cfe5a1279111
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-07-30 09:46:58 +1000

    fix: expose normalized-safe selection execution

--- packages/selection/src/index.ts
+++ packages/selection/src/index.ts
@@ -2,5 +2,5 @@ export * from "./types.js";
 export * from "./public-validation.js";
 export * from "./features.js";
 export * from "./compare.js";
-export * from "./select.js";
+export * from "./public-select.js";
 export * from "./promotion.js";
