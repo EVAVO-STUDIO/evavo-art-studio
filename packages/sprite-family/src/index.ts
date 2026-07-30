235601789886fe88db4ab4c3dcd1684616ba5cf4
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-07-30 12:54:37 +1000

    refactor: route sprite family verification through strict facade

--- packages/sprite-family/src/index.ts
+++ packages/sprite-family/src/index.ts
@@ -1,4 +1,4 @@
 export * from "./types.js";
 export * from "./validation.js";
 export * from "./composite.js";
-export * from "./verify.js";
+export { verifySpriteFamily } from "./public-verify.js";
