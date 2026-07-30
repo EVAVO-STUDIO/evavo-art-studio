69f546ea29310ab1b585fc32d5767b5ff40f7c29
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-07-30 16:18:41 +1000

    feat: export targeted repair job compiler

--- packages/repair/src/index.ts
+++ packages/repair/src/index.ts
@@ -2,4 +2,5 @@ export * from "./types.js";
 export * from "./validation.js";
 export * from "./classify.js";
 export * from "./execution.js";
+export * from "./jobs.js";
 export { planTargetedRepair } from "./public-planner.js";
