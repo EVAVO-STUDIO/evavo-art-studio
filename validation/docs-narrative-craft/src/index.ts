24016b19b19a434f498c380ea20ae3e70dfe30ad
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-08-04 18:42:37 +1000

    test(validation): bind direct runtime exports

--- validation/docs-narrative-craft/src/index.ts
+++ validation/docs-narrative-craft/src/index.ts
@@ -1,10 +1,10 @@
 export * from "./book-studio-narrative-craft-types";
+export { BOOK_NARRATIVE_ARCHETYPES } from "./book-studio-narrative-craft-archetypes";
+export { BOOK_NARRATIVE_KNOWLEDGE_MODULES } from "./book-studio-narrative-craft-knowledge-modules";
 export {
-  BOOK_NARRATIVE_ARCHETYPES,
-  BOOK_NARRATIVE_KNOWLEDGE_MODULES,
   BOOK_NARRATIVE_QUALITY_RUBRIC,
   BOOK_NARRATIVE_REVISION_PROTOCOL,
-} from "./book-studio-narrative-craft-knowledge";
+} from "./book-studio-narrative-craft-quality";
 export {
   bookAuthoringOperationRequiresNarrativeCraft,
   listBookNarrativeCraftCapabilities,
@@ -15,6 +15,4 @@ export {
   fingerprintBookNarrativeCraftPacket,
   validateBookNarrativeCraftPacket,
 } from "./book-studio-narrative-craft-packet";
-export {
-  evaluateBookNarrativeCraftEvidence,
-} from "./book-studio-narrative-craft-evaluate";
+export { evaluateBookNarrativeCraftEvidence } from "./book-studio-narrative-craft-evaluate";
