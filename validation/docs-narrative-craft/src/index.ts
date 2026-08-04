1d10f538a495150c0a0b52324efa11bc0c273478
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-08-04 18:40:43 +1000

    test(validation): expose explicit runtime exports

--- validation/docs-narrative-craft/src/index.ts
+++ validation/docs-narrative-craft/src/index.ts
@@ -1,8 +1,20 @@
 export * from "./book-studio-narrative-craft-types";
-export * from "./book-studio-narrative-craft-knowledge";
-export * from "./book-studio-narrative-craft-archetype";
-export * from "./book-studio-narrative-craft-state";
-export * from "./book-studio-narrative-craft-cross-field";
-export * from "./book-studio-narrative-craft-provider";
-export * from "./book-studio-narrative-craft-packet";
-export * from "./book-studio-narrative-craft-evaluate";
+export {
+  BOOK_NARRATIVE_ARCHETYPES,
+  BOOK_NARRATIVE_KNOWLEDGE_MODULES,
+  BOOK_NARRATIVE_QUALITY_RUBRIC,
+  BOOK_NARRATIVE_REVISION_PROTOCOL,
+} from "./book-studio-narrative-craft-knowledge";
+export {
+  bookAuthoringOperationRequiresNarrativeCraft,
+  listBookNarrativeCraftCapabilities,
+  narrativeCraftModeForBookAuthoringOperation,
+} from "./book-studio-narrative-craft-provider";
+export {
+  compileBookNarrativeCraftPacket,
+  fingerprintBookNarrativeCraftPacket,
+  validateBookNarrativeCraftPacket,
+} from "./book-studio-narrative-craft-packet";
+export {
+  evaluateBookNarrativeCraftEvidence,
+} from "./book-studio-narrative-craft-evaluate";
