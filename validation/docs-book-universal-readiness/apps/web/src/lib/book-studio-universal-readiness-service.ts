d31003708e43bb39ead0df0dc5c6513887dda51e
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-08-04 15:27:28 +1000

    test(validation): mirror readiness server service

---
+++ validation/docs-book-universal-readiness/apps/web/src/lib/book-studio-universal-readiness-service.ts
@@ -0,0 +1,10 @@
+import "server-only";
+
+export {
+  BOOK_UNIVERSAL_READINESS_CONTRACT,
+  SUPPORTED_BOOK_CONTENT_CLASSES,
+} from "../../../../packages/core/src/book-studio-universal-readiness";
+
+export {
+  compileGovernedBookUniversalReadiness as compileBookUniversalReadiness,
+} from "../../../../packages/core/src/book-studio-universal-readiness-governance";
