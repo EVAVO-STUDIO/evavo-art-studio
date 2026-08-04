4412cc0a1c52404e63af56b5af74e4f17301357e
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-08-04 18:23:58 +1000

    test(validation): mirror Docs authoring operation contract

---
+++ validation/docs-narrative-craft/src/book-studio-authoring-types.ts
@@ -0,0 +1,11 @@
+export type BookAuthoringOperation =
+  | "draft_candidate"
+  | "revise_candidate"
+  | "critique_candidate"
+  | "evaluate_voice"
+  | "fact_check_candidate"
+  | "continuity_review"
+  | "line_edit_candidate"
+  | "copyedit_candidate"
+  | "proofread_candidate"
+  | "custom";
