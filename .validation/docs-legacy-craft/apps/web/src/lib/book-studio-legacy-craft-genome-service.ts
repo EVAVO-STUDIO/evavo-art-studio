d7a292e7294338518f43c5554084d3687f2c4ab1
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-08-05 02:26:45 +1000

    test(validation): mirror exact Docs legacy craft service

---
+++ .validation/docs-legacy-craft/apps/web/src/lib/book-studio-legacy-craft-genome-service.ts
@@ -0,0 +1,11 @@
+import "server-only";
+
+export {
+  BOOK_LEGACY_CRAFT_GENOME_CONTRACT,
+  executeBookLegacyCraftGenomeRequest,
+  listBookLegacyCraftGenomeCapabilities,
+  validateBookLegacyCraftGenomeRequest,
+  type BookLegacyCraftGenomeOperation,
+  type BookLegacyCraftGenomeRequestV1,
+  type BookLegacyCraftGenomeResultV1,
+} from "../../../../packages/core/src/book-studio-legacy-craft-genome";
