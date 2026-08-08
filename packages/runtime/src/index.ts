dcf6f1c547ed59b51dabeacd19e110f6cb8bb8c2
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-08-08 15:52:22 +1000

    fix(runtime): export hardened repository boundary

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -1,6 +1,6 @@
 export * from "./types.js";
 export * from "./normalize.js";
 export * from "./journal.js";
-export * from "./local-repository.js";
+export { LocalRuntimeRepository } from "./local-repository-boundary.js";
 export * from "./worker.js";
 export * from "./pg-boss-delivery.js";
