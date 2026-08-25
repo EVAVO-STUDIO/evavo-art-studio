ffe33cc486a82d7208e11edeb5805d31c365cbf1
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-08-25 20:06:19 +1000

    feat: export animation provider work-order compiler

--- packages/art-direction/src/index.ts
+++ packages/art-direction/src/index.ts
@@ -11,3 +11,4 @@ export { compileArtDirectionJob } from "./public-jobs.js";
 export * from "./layered-production.js";
 export * from "./art-production-orchestrator.js";
 export * from "./animation-director.js";
+export * from "./animation-provider-work-orders.js";
