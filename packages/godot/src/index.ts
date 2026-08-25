99ac58e51888213246f3f9d6adcad78f1dad34c8
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-08-25 20:17:16 +1000

    feat: export Godot animation descriptor acceptance

--- packages/godot/src/index.ts
+++ packages/godot/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./types.js";
 export * from "./path.js";
 export * from "./descriptor.js";
+export * from "./animation-acceptance.js";
 export * from "./importer-script.js";
 export * from "./write.js";
 export * from "./run.js";
