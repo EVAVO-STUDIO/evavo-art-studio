b94a7c21704e03ee96ffbb86118289a48806b4c4
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-08-26 00:25:55 +1000

    feat: export animation anatomy repair pipeline

--- packages/repair/src/index.ts
+++ packages/repair/src/index.ts
@@ -28,4 +28,6 @@ export * from "./animation-motion-repair.js";
 export * from "./animation-motion-repair-provider.js";
 export * from "./animation-appearance-repair.js";
 export * from "./animation-appearance-repair-provider.js";
+export * from "./animation-anatomy-repair.js";
+export * from "./animation-anatomy-repair-provider.js";
 export { planTargetedRepair } from "./public-planner.js";
