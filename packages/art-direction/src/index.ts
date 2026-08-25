8752847a476ea7a5d61309df0474dcafcd2de17e
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-08-25 18:52:18 +1000

    feat: export animation director compiler

--- packages/art-direction/src/index.ts
+++ packages/art-direction/src/index.ts
@@ -10,3 +10,4 @@ export { ART_DIRECTION_COMPILE_CAPABILITIES } from "./jobs.js";
 export { compileArtDirectionJob } from "./public-jobs.js";
 export * from "./layered-production.js";
 export * from "./art-production-orchestrator.js";
+export * from "./animation-director.js";
