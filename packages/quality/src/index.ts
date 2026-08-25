4a04cae27f5e4c707fdad15362c61e825e160699
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-08-25 19:01:46 +1000

    feat: export animation motion quality analysis

--- packages/quality/src/index.ts
+++ packages/quality/src/index.ts
@@ -3,4 +3,5 @@ export * from "./validation.js";
 export * from "./decode.js";
 export * from "./analyse-frame.js";
 export * from "./analyse-sequence.js";
+export * from "./analyse-motion.js";
 export * from "./sequence-files.js";
