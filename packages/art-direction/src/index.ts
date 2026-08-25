555f01164581606f1efe537e2395bcc425bf508d
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-08-26 00:27:58 +1000

    feat: export deterministic walk pose template

--- packages/art-direction/src/index.ts
+++ packages/art-direction/src/index.ts
@@ -15,3 +15,4 @@ export * from "./animation-routing.js";
 export * from "./animation-cel-handoff.js";
 export * from "./animation-pose-control.js";
 export * from "./animation-pose-control-verification.js";
+export * from "./animation-walk-pose-template.js";
