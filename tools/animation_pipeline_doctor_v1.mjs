d5fc186fdc48410c99929923cea90b66c80f700d
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-08-30 23:55:33 +1000

    feat(animation): add cross-studio pipeline doctor

---
+++ tools/animation_pipeline_doctor_v1.mjs
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+
+export { ANIMATION_PIPELINE_DOCTOR_VERSION, ANIMATION_PIPELINE_DOCTOR_SCHEMA, animationPipelineDoctorSha256, inspectAnimationPipelineV1, planAnimationPipelineRepairsV1, verifyAnimationPipelineV1 } from "./animation_pipeline_doctor_v1_internal.mjs";
