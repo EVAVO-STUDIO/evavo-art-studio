014ce1e20250feaa52c83cde554a34a64dabedf7
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-09-06 09:10:43 +1000

    feat: export existing image difference proof

--- packages/media/src/index.ts
+++ packages/media/src/index.ts
@@ -18,6 +18,7 @@ export * from "./aseprite-interchange-result.js";
 export * from "./pose-control-render.js";
 export * from "./finishing-pass.js";
 export * from "./preservation-polish.js";
+export * from "./existing-image-diff.js";
 export * from "./compositing-pass.js";
 export * from "./effect-layer.js";
 export * from "./effect-presets.js";
