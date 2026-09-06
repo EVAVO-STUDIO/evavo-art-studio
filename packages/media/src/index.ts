d14c7ae12e70479c7d3faa027a4329da7289187f
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-09-06 10:55:25 +1000

    Export independent enhancement local-detail review

--- packages/media/src/index.ts
+++ packages/media/src/index.ts
@@ -26,6 +26,7 @@ export * from "./image-review-profiles.js";
 export * from "./image-similarity.js";
 export * from "./image-review-orchestrator.js";
 export * from "./enhancement-review-bridge.js";
+export * from "./enhancement-local-detail-risk.js";
 export * from "./enhancement-review-session.js";
 export * from "./localized-edit.js";
 export * from "./edit-mask.js";
