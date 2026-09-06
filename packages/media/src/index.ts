a29e4d8fac927c427675f34630d9bd8f0dae9fa6
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-09-06 10:40:38 +1000

    Export Work header approval packet

--- packages/media/src/index.ts
+++ packages/media/src/index.ts
@@ -35,6 +35,7 @@ export * from "./work-header-review-lineage.js";
 export * from "./work-header-visual-critique.js";
 export * from "./work-header-selection-resolver.js";
 export * from "./work-header-page-render-review.js";
+export * from "./work-header-approval-packet.js";
 export * from "./work-page-media-review.js";
 export * from "./compositing-pass.js";
 export * from "./effect-layer.js";
