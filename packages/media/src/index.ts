ce8cf9aa1a079db9c80cc4eb352738e5c6f0c164
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-09-09 10:34:17 +1000

    feat: export image repair routing

--- packages/media/src/index.ts
+++ packages/media/src/index.ts
@@ -29,6 +29,7 @@ export * from "./image-artifact-signals.js";
 export * from "./image-review-profiles.js";
 export * from "./image-similarity.js";
 export * from "./image-review-orchestrator.js";
+export * from "./image-repair-routing.js";
 export * from "./enhancement-review-bridge.js";
 export * from "./enhancement-local-detail-risk.js";
 export * from "./enhancement-review-session.js";
