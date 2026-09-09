363eb9b1f006214fdec020592649cceec091e2ab
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-09-09 13:29:53 +1000

    feat: export texture preprocessing primitives

--- packages/media/src/index.ts
+++ packages/media/src/index.ts
@@ -35,6 +35,7 @@ export * from "./texture-map-review.js";
 export * from "./texture-tile-proof.js";
 export * from "./texture-set-review.js";
 export * from "./texture-channel-pack.js";
+export * from "./texture-preprocess.js";
 export * from "./uv-layout-review.js";
 export * from "./wavefront-obj-uv.js";
 export * from "./enhancement-review-bridge.js";
